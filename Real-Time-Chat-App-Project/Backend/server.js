45531c32cb716379e092d1477223481a149e8570
Nayan Kunwar <nayankunwar678@gmail.com>
2024-03-20 01:57:59 +0530

    server and client running on same port | build frontend

--- Real-Time-Chat-App-Project/Backend/server.js
+++ Real-Time-Chat-App-Project/Backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,7 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 dotenv.config();
 
 //Middleware
@@ -21,6 +23,12 @@ app.use(cookieParser()); // Middleware parses the Cookie header from the incomin
 app.use("/api/auth", authRoutes); // Contain Authentication Routes.
 app.use("/api/messages", messageRoutes); // Contain Messages Routes.
 app.use("/api/users", userRoutes); // Contain Users Routes.
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
